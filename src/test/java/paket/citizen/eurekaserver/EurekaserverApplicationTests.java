6aa709e96f738029188b00c9e35d30b94e05431f
Samet Dağ <samet822@hotmail.com>
2020-09-19 14:58:21 +0300

    Initial commit

---
+++ src/test/java/paket/citizen/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package paket.citizen.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
